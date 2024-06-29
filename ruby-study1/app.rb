bcbc267289fa24f5f7d964713d28c34a36db19ee
ryushi-masa <ohaoha.0610@gmail.com>
2024-06-29 11:53:40 +0900

    文字列、数値の扱い方
    条件分岐の扱い方
    
    繰り返しの扱い方

--- ruby-study1/app.rb
+++ ruby-study1/app.rb
@@ -1,5 +1,37 @@
+# 文字列、数値の扱い方
 def hello_ruby
   puts 'Hello Ruby!'
+  puts "こんにちは Ruby!"
+  puts 1 + 3
+  puts 10 / 3
+  puts 10 % 3
 end
 
 hello_ruby
+
+x = 10
+y = 20
+result = x + y
+# 結果を出力
+puts result
+
+# 条件分岐の扱い方
+a = 10
+if a >= 10 then
+  puts('aは10以上の数値です')
+else
+  puts('aは9以下の数値です')
+end
+
+# 繰り返しの扱い方
+i = 0
+while i <= 10
+  p(i)
+  i += 1
+end
+
+y = 11
+10.times do
+  puts(y)
+  y += 1
+end
